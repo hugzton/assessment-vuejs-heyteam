f4203bd173a810c19a293e69cfe7d7d2f6d42408
Hugo Toussaint <hugo.toussaint@epitech.eu>
2020-07-31 14:26:07 +0200

    add buttn add item & delete item

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="app">
-    <ul>   
-      <div 
-          v-for="item in items[0]" 
-          v-bind:key="item.index"
-          :style="handleColor(item)"
-        >
-        <input v-model="item.color" />
-        <button>X</button>
-      </div>  
-      <button>+</button>
+    <ul>
+      <div class="column">
+        <div
+            class="card"
+            v-for="item in items[0]" 
+            v-bind:key="item.index"
+            :style="handleColor(item)"
+            v-on:click="handleSelect(item)"
+          >
+          <input style=" width: 25%;" v-model="item.color" />
+          <button class="btn-card-delete">X</button>
+        </div>  
+        <button class="btn-card-add" v-on:click="addItem(0, items[0])">+</button>
+      </div>
+    </ul>
+    <ul>
+      <div class="column">   
+        <div 
+            class="card"
+            v-for="item in items[1]" 
+            v-bind:key="item.index"
+            :style="handleColor(item)"
+            v-on:click="handleSelect(item)"
+          >
+          <input style=" width: 25%;" v-model="item.color" />
+          <button class="btn-card-delete">X</button>
+        </div>  
+        <button class="btn-card-add" v-on:click="addItem(1, items[1])">+</button>
+      </div>
     </ul>
   </div>
-</template>
+</template>okokok
 
 <script>
 
@@ -49,7 +68,14 @@ export default {
   methods: {
     handleColor (item) {
       return "background-color: " + item.color
-    }
+    },
+    handleSelect (item) {
+      this.isSelected = item
+      console.log(this.isSelected);
+    },
+    addItem(id, items) {
+        items.push({ list: id, color: "Orange" })
+    },
   }
 }
 </script>
@@ -63,4 +89,38 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.list {
+  border: 1px solid black;
+}
+.btn-card-delete {
+  top:0; 
+  right:0;
+  z-index :1;
+}
+
+.btn-card-add {
+  top:0; 
+  right:0;
+  z-index :1;
+}
+
+#itm {
+  position: relative;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  text-align: center;
+  background-color: #f1f1f1;
+}
+
+/* Float four columns side by side */
+.column {
+  float: left;
+  width: 25%;
+  padding: 0 10px;
+}
+
 </style>
